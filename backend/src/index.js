48b87a21070883c3a896fc0adcba59594181a67a
deepaksharebite <109091606+deepaksharebite@users.noreply.github.com>
2024-12-09 14:52:43 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import morgan from "morgan";
 
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
@@ -14,6 +15,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 const port = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -27,6 +29,14 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.use(errorHandler);
 
